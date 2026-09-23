3340e61cfc8035a2010a97164f04be60f94d6577
JUANH2320 <juan-jose3340@outlook.com>
2026-09-23 03:30:40 -0600

    test: forzar fallo en pruebas unitarias

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const { sumar } = require("./app.js");
 
-// Prueba unitaria básica
-assert.strictEqual(sumar(2, 3), 5, "La suma de 2 + 3 debe ser 5");
+// Error intencional: forzamos que espere 99 en lugar de 5
+assert.strictEqual(sumar(2, 3), 99, "La suma de 2 + 3 debe ser 5");
 console.log("Pruebas unitarias pasaron exitosamente.");
